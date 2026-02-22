d9aed452797a2ed57f8e1a806a6afd6928157bcd
Nelia Mann <mannn@union.edu>
2026-02-22 21:00:09 +0000

    some basic utilities added for hass

---
+++ src/shared-resources/util/hass-util.js
@@ -0,0 +1,44 @@
+function getHassEntities(hass) {
+    return hass.entities;
+}
+
+function getHassStates(hass) {
+    return hass.states;
+}
+
+function getEntity(hass, entityId) {
+    return getHassEntities(hass)[entityId];
+}
+
+function getState(hass, entityId) {
+    return getHassStates(hass)[entityId];
+}
+
+function isLight(hass, entityId) {
+    const entity = this.getEntity(hass, entityId);
+    const notLight = entity.labels.includes('not_light');
+    return (entityId.substring(0, 6) === "light.") && (!notLight);
+}
+
+function getLightIds(hass) {
+    const entities = getHassEntities(hass);
+    const lightIds = Object.keys(entities).filter((entityId) =>
+        isLight(hass, entityId));
+    return new Set(lightIds);
+}
+
+function isTheme(entityId) {
+    return ((entityId.substring(0, 7) === "select.") && (entityId.includes("theme")))
+}
+
+function getThemeIds(hass) {
+    const entities = this.getHassEntities(hass);
+    const themeIds = Object.keys(entities).filter((entityId) =>
+        isTheme(hass, entityId));
+    return new Set(themeIds);
+}
+
+export {
+    getLightIds,
+    getThemeIds
+}
