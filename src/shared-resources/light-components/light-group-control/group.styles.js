c977afdc3c317e51636e92e102e730578ac73b57
Nelia Mann <mannn@union.edu>
2026-03-03 18:15:58 +0000

    debug popout

--- src/shared-resources/light-components/light-group-control/group.styles.js
+++ src/shared-resources/light-components/light-group-control/group.styles.js
@@ -3,6 +3,8 @@
 export default css`
 
     :host {
+        flex: 1;
+        min-height: 0;
         height: var(--light-group-height, 100%);
         width: var(--light-group-width, 100%);
         display: flex;
