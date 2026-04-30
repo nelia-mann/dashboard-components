d1332d52563bc90a1d5932b8ff9ad3b124faa333
Nelia Mann <mannn@union.edu>
2026-04-30 20:24:57 +0000

    still debugging areas

--- src/lighting-card/main/main.js
+++ src/lighting-card/main/main.js
@@ -57,20 +57,19 @@ export class LightingCard extends HaMainComponent {
     setStructure() {
         const floors = this.getHassFloors();
         Object.keys(floors).forEach((floorId) => {
-        const filteredIds = this.filterEntityIdsForFloor(this.getEntityIds(), floorId);
-           const soloLightIds = [...filteredIds].filter((entityId) => isSoloLight(this.getHass(), entityId));
-            if (filteredIds.size > 0) {
-                const floorDictionary = {
-                    name: this.getHassFloorName(floorId),
-                    structure: {},
-                    entityIds: filteredIds,
-                    soloLightIds: new Set(soloLightIds)
-                }
-                this.setSpecialStructure(floorDictionary);
-                this.getStructure()[floorId] = floorDictionary;
-            }
+            const filteredIds = this.filterEntityIdsForFloor(this.getEntityIds(), floorId);
+            /*            const soloLightIds = [...filteredIds].filter((entityId) => isSoloLight(this.getHass(), entityId));
+                        if (filteredIds.size > 0) {
+                            const floorDictionary = {
+                                name: this.getHassFloorName(floorId),
+                                structure: {},
+                                entityIds: filteredIds,
+                                soloLightIds: new Set(soloLightIds)
+                            }
+                            this.setSpecialStructure(floorDictionary);
+                            this.getStructure()[floorId] = floorDictionary;
+                        } */
         })
-
     }
 
     setSpecialStructure(floorDictionary) {
