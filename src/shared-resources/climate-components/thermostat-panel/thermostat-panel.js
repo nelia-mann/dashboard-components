a2aaa993082cfbef64c412ee30aada21698c2f3b
Nelia Mann <mannn@union.edu>
2026-06-10 15:26:01 -0400

    debugging thermostat issue with heat_cool

--- src/shared-resources/climate-components/thermostat-panel/thermostat-panel.js
+++ src/shared-resources/climate-components/thermostat-panel/thermostat-panel.js
@@ -107,7 +107,6 @@ export class ThermostatPanel extends HaClimateComponent {
 
     render() {
         if (this.isInitialized()) {
-            console.log(this.getSliderStructure());
             return html`
                 <double-circular-slider
                     .changedEntityIds = ${this.getCEIs()}
