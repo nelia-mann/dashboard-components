e857bf8bcfe7796d08205e03204a724c5dc91561
Nelia Mann <mannn@union.edu>
2026-06-10 15:16:14 -0400

    debugging thermostat issue with heat_cool

--- src/shared-resources/climate-components/thermostat-panel/thermostat-panel.js
+++ src/shared-resources/climate-components/thermostat-panel/thermostat-panel.js
@@ -51,7 +51,7 @@ export class ThermostatPanel extends HaClimateComponent {
         if (this.getMode() === 'cool') {
             structure.maxValue = this.getTarget();
         }
-        if  (this.getMode() === 'auto') {
+        if  (this.getMode() === 'heat_cool') {
             structure.targetValue = this.getTarget();
         }
         return structure;
